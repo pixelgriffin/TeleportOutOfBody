23c0b9823b27536505769b1954a15a8b070a2a06
Nathan <navarrogriffin@gmail.com>
2018-03-02 15:56:24 -0800

    Initial commit

---
+++ Assets/MimicHand.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class MimicHand : MonoBehaviour {
+
+    public Hand hand;
+
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update () {
+        this.transform.localPosition = hand.transform.localPosition - new Vector3(player.GetComponent<Player>().hmdTransform.localPosition.x, this.transform.parent.position.y, player.GetComponent<Player>().hmdTransform.localPosition.z);
+        this.transform.localRotation = hand.transform.localRotation;
+
+        this.transform.localPosition += new Vector3(0, this.transform.parent.position.y, 0);
+	}
+}
---
+++ Assets/RotateOppositeHead.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class RotateOppositeHead : MonoBehaviour {
+
+    private Player player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(player.hmdTransform.eulerAngles.y + " + 180");
+        this.transform.rotation = Quaternion.Euler(new Vector3(0, player.hmdTransform.eulerAngles.y, 0));
+	}
+}
