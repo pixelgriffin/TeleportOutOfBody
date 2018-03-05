5b211c119911e2a069cd2b4002d15fd037d04b8c
Nathan <navarrogriffin@gmail.com>
2018-03-05 14:26:15 -0800

    Fixed body physics and added ProBuilder

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+    public Animator anim;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        anim.SetBool("Jump", false);
+    }
+}
